8c28a01df27f1682dd6de701e5724a1568188ada
Sabri <sabri@mykolab.com>
2015-05-06 22:39:18 +0200

    Cosmetic

--- usbkill.py
+++ usbkill.py
@@ -1,17 +1,26 @@
-#	 This program is free software: you can redistribute it and/or modify
-#	 it under the terms of the GNU General Public License as published by
-#	 the Free Software Foundation, either version 3 of the License, or
-#	 (at your option) any later version.
- 
-#	 This program is distributed in the hope that it will be useful,
-#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	 GNU General Public License for more details.
-
-#	 You should have received a copy of the GNU General Public License
-#	 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Contact: hephaestos@riseup.net - 8764 EF6F D5C1 7838 8D10 E061 CF84 9CE5 42D0 B12B
+#             _     _     _ _ _  
+#            | |   | |   (_) | | 
+#  _   _  ___| |__ | |  _ _| | | 
+# | | | |/___)  _ \| |_/ ) | | | 
+# | |_| |___ | |_) )  _ (| | | | 
+# |____/(___/|____/|_| \_)_|\_)_)
+#
+#
+# Copyright (C) 2015 Hephaestos <hephaestos@riseup.net> (8764 EF6F D5C1 7838 8D10 E061 CF84 9CE5 42D0 B12B)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 import re
 import subprocess
