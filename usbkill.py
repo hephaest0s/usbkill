73419170b093c58ea05bc551d8db73fa32f30d44
David Norman <deekayen@deekayen.net>
2015-05-06 10:48:29 -0400

    Add killall loginwindow Finder to Darwin halt.

--- usbkill.py
+++ usbkill.py
@@ -49,8 +49,8 @@ def kill_computer():
 	
 	# Poweroff computer immediately
 	if CURRENT_PLATFORM.startswith("DARWIN"):
-		# OS X (Darwin) - Will halt ungracefully, without notifying apps
-		os.system("halt -q")
+		# OS X (Darwin) - Will halt ungracefully, without signaling apps
+		os.system("killall loginwindow Finder && halt -q")
 	elif CURRENT_PLATFORM.endswith("BSD"):
 		# BSD-based systems - Will shutdown
 		os.system("shutdown -h now")
