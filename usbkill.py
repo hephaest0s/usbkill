f1c111cea87adf6b9657371ebd0f26ecd51f939d
Sabri <sabri@mykolab.com>
2015-05-09 23:20:30 +0200

    Attempt to fix https://github.com/hephaest0s/usbkill/issues/53

--- usbkill.py
+++ usbkill.py
@@ -391,9 +391,8 @@ def startup_checks():
 
 	# Warn the user if he does not have FileVault
 	if CURRENT_PLATFORM.startswith("DARWIN"):
-	
 		try:
-			if subprocess.check_output(["/usr/bin/fdesetup", "isactive"]).decode('utf-8').strip() != "true":
+			if subprocess.check_output(["/usr/bin/fdesetup", "status"]).decode('utf-8').strip() != "FileVault is On.":
 				print("[NOTICE] FileVault is disabled. Sensitive data SHOULD be encrypted.")
 		except subprocess.CalledProcessError as e:
 			print(e)
