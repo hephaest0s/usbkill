75957ea2b0fc4a687d38ce7ec7492e75b4dd1fe1
Sabri <sabri@mykolab.com>
2015-05-09 22:26:10 +0200

    Typo

--- usbkill.py
+++ usbkill.py
@@ -352,7 +352,7 @@ def startup_checks():
 
 	# Warn the user if he does not have FileVault
 	if CURRENT_PLATFORM.startswith("DARWIN"):
-		if subprocess.check_output("/usr/bin/fdesetup isactive", shell=True).strip() != "true":
+		if subprocess.check_output("/usr/bin/fdesetup isactive", shell=True).decode('utf-8').strip() != "true":
 			print("[NOTICE] FileVault is disabled. Sensitive data SHOULD be encrypted.")
 
 	if not os.path.isdir("/etc/usbkill/"):
